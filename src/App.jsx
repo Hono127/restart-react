facbc9c8bc1ef67eea98bc92b5086468739caed2
小野寛生 <hiro85fsc6@gmail.com>
2022-12-05 23:28:45 +0900

    ### Udemyでreact再勉強 - 機能実装は完了 - ちょいちょい関数とStateを見失って苦戦した - 「戻す」機能だけ自分で実装してみたが、案外躓いて結局見た

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,8 @@ import "./style.css";
 export const App = () => {
   const [todoText, setTodoText] = useState('');
   const [inCompleteTodos, setInCompleteTodos] = useState([
-    "あああ",
-    "いいい"
   ]);
   const [completeTodos, setCompleteTodos] = useState([
-    "ううう",
-    "えええ"
   ]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
@@ -22,21 +18,39 @@ export const App = () => {
 
   const onClickDelete = (index) => {
     const newTodos = [...inCompleteTodos];
-    newTodos.splice(index, 1);
+    newTodos.splice(index, 1); // spliceは(何番目の,何個)の要素を削除するという意味
     setInCompleteTodos(newTodos);
   };
 
   const onClickComplete = (index) => {
+    const newInCompleteTodos = [...inCompleteTodos];
+    newInCompleteTodos.splice(index, 1); // spliceは(何番目の,何個)の要素を削除するという意味
+    const newCompleteTodos = [...completeTodos, inCompleteTodos[index]];
+    setInCompleteTodos(newInCompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
 
+  const onClickReturn = (index) => {
+    // 完了エリアから削除する
+    const newCompleteTodos = [...completeTodos]; // 今完了エリアにいるタスクを取得
+    newCompleteTodos.splice(index, 1);
+
+    const newInCompleteTodos = [...inCompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setInCompleteTodos(newInCompleteTodos);
   };
   return (
     <>
+      {/* ==========================================追加エリア================================================ */}
       <div className="add_todo_area">
         <input id="add-text" placeholder="TODOを入力" type="text" value={todoText} onChange={onChangeTodoText}></input>
         <button id="add-button" onClick={onClickAdd}>追加</button>
       </div>
 
+
+
       <div className="incomplete_area content_area">
+        {/* ==========================================未完了エリア================================================ */}
         <h2>未完了のTODO</h2>
         <ul id="inComplete_list">
           {inCompleteTodos.map((todo, index) => {
@@ -51,14 +65,16 @@ export const App = () => {
         </ul>
       </div>
 
+
+      {/* ==========================================完了エリア================================================ */}
       <div className="complete_area content_area2">
         <h2>完了したTODO</h2>
         <ul className="complete_list">
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <li key={todo} className="list_item">
                 <p className="list_name">{todo}</p>
-                <button>戻す</button>
+                <button onClick={() => onClickReturn(index)}>戻す</button>
               </li>
             )
           })}
