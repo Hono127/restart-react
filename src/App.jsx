9a81ea6a89db51bdc21f5412d4daf6cd357cc973
小野寛生 <hiro85fsc6@gmail.com>
2022-12-08 21:22:05 +0900

    ### React再勉強 - コンポーネント化 - Stateがどんなものか、どういうロジックか段々理解できてきた（思い出してきた）

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { InCompleteTodo } from './components/InCompleteTodo';
+import { InputTodo } from './components/InputTodo';
 import "./style.css";
 
 export const App = () => {
@@ -41,45 +43,20 @@ export const App = () => {
   };
   return (
     <>
-      {/* ==========================================追加エリア================================================ */}
-      <div className="add_todo_area">
-        <input id="add-text" placeholder="TODOを入力" type="text" value={todoText} onChange={onChangeTodoText}></input>
-        <button id="add-button" onClick={onClickAdd}>追加</button>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onClick={onClickAdd}
+        onChange={onChangeTodoText} />
 
+      <InCompleteTodo
+        todos={inCompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete} />
 
-
-      <div className="incomplete_area content_area">
-        {/* ==========================================未完了エリア================================================ */}
-        <h2>未完了のTODO</h2>
-        <ul id="inComplete_list">
-          {inCompleteTodos.map((todo, index) => {
-            return (
-              <li key={todo} className="list_item">
-                <p className="list_name">{todo}</p>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
 
 
       {/* ==========================================完了エリア================================================ */}
-      <div className="complete_area content_area2">
-        <h2>完了したTODO</h2>
-        <ul className="complete_list">
-          {completeTodos.map((todo, index) => {
-            return (
-              <li key={todo} className="list_item">
-                <p className="list_name">{todo}</p>
-                <button onClick={() => onClickReturn(index)}>戻す</button>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+
     </>
   )
 };
